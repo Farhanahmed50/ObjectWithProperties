be41432c924b4a2c0d9869d3c130b214beca1a4e
Farhan Ahmed <mfarhanahmed40@gmail.com>
2021-07-25 16:01:52 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var obj = {
+    name: "Farhan Ahmed",
+    email: "mfarhanahmed40@gmail.com",
+    password: "nahraf321",
+    age: "21",
+    gender: "Male",
+    city: "Karachi",
+    country: "Pakistan"
+}
+
+
+if ('age' in obj) {
+    console.log("This property is exist");
+} else {
+    console.log("This property is not exist");  
+}
